16d6ce384dd74c60164226df5aceb913cab4b6cc
Evgeniy Volkov <volkovev94@yandex.ru>
2023-08-13 10:40:39 +0300

    PRACTICUM-3

---
+++ api_yamdb/api/permissons.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class AuthorOrReadPermission(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+User = get_user_model()
 
 
 class Category(models.Model):
@@ -27,3 +32,46 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Review(models.Model):
+    text = models.TextField('Отзыв')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Title,
+        on_delete=models.CASCADE,
+        related_name='review',
+        null=True
+    )
+    score = models.IntegerField(
+        related_name='rating',
+        validators=[
+            MinValueValidator(1,
+                              message='Упс, оценка ниже допустимой'),
+            MaxValueValidator(10,
+                              message='Упс, оценка выше допустимой'),
+        ]
+    )
+
+    class Meta:
+        ordering = ('pub_date',)
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'отзывы'
+
+
+class Comment(models.Model):
+    text = models.TextField('Комментарии')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(
+        Review,
+        on_delete=models.CASCADE,
+        related_name='comments',
+        null=True
+    )
+
+    class Meta:
+        ordering = ('pub_date',)
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'комментарии'
