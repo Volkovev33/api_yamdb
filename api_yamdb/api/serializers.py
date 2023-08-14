1717932c3411ccbd66e7ecf21acce09e1d150e73
Denis Gabidulin <gabidulin.denis@yandex.ru>
2023-08-14 20:01:33 +0300

    PRACTICUM-7

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import datetime as dt
 
 from rest_framework import serializers
@@ -81,49 +80,3 @@ class CommentSerializer(serializers.ModelSerializer):
         model = Comment
         fields = (
             'id', 'text', 'author', 'pub_date')
-=======
-import datetime as dt
-
-from rest_framework import serializers
-
-from reviews.models import Category, Genre, Title
-
-
-class CategorySerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Category
-        fields = '__all__'
-        lookup_field = 'slug'
-
-
-class GenreSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Genre
-        fields = '__all__'
-        lookup_field = 'slug'
-
-
-class TitleSerializer(serializers.ModelSerializer):
-    category = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Category.objects.all()
-    )
-    genre = serializers.SlugRelatedField(
-        slug_field='slug',
-        queryset=Genre.objects.all(),
-        many=True
-    )
-
-    class Meta:
-        model = Title
-        fields = '__all__'
-
-    def validate_year(self, value):
-        year = dt.date.today().year
-        if not (value <= year):
-            raise serializers.ValidationError(
-                'Нельзя добавлять произведения, которые еще не вышли')
-        return value
->>>>>>> feature/PRACTICUM-7
